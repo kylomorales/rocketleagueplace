1959423edc884a5f5e4eb83731c9aca9bb634d82
kylomorales <102863617+kylomorales@users.noreply.github.com>
2022-04-02 23:32:42 +0100

    bugged but fixed

--- egg_people_unite_overlay.user.js
+++ egg_people_unite_overlay.user.js
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/847819344805494794/959940932864704532/hopefully_final_overlay.png";
+            i.src = "https://cdn.discordapp.com/attachments/847819344805494794/959943341192138852/hopefully_final_overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
