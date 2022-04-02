67c1ac8f6c2a146caa6151b771eaaa35a25ad86d
kylomorales <102863617+kylomorales@users.noreply.github.com>
2022-04-03 00:28:01 +0100

    This could be the one

--- egg_people_unite_overlay.user.js
+++ egg_people_unite_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         April Fools r/RocketLeague Allies
 // @namespace    http://tampermonkey.net/
-// @version      3.4
+// @version      3.5
 // @description  Keep the canvas beautiful!
 // @author       Adapted by u/kylomorales for r/RocketLeague, Original author oralekin from osu! /r/osuplace, Rocket League template by u/WestonHawk
 // @match        https://hot-potato.reddit.com/embed*
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/847819344805494794/959950790599016488/actual_final_overlay.png";
+            i.src = "https://github.com/kylomorales/rocketleagueplace/raw/main/new_overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
