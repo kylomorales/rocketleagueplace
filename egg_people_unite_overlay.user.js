9cebeaa84620dd6f28ee9eb9d29452d41646c335
Weston Hawk <weston.pitman@gmail.com>
2022-04-02 15:26:28 -0700

    Update egg_people_unite_overlay.user.js

--- egg_people_unite_overlay.user.js
+++ egg_people_unite_overlay.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      3.0
 // @description  Keep the canvas beautiful!
-// @author       Adapted by u/kylomorales for r/RocketLeague, Original author oralekin from osu! /r/osuplace
+// @author       Adapted by u/kylomorales for r/RocketLeague, Original author oralekin from osu! /r/osuplace, Rocket League template by u/WestonHawk
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @updateURL    https://github.com/kylomorales/rocketleagueplace/raw/main/egg_people_unite_overlay.user.js
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/847819344805494794/959891600379637811/output_overlay.png";
+            i.src = "https://i.imgur.com/iEuhtCt.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
