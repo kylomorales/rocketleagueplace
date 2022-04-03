ad8fade432972f2549138c83af7eb220924bc68d
kylomorales <102863617+kylomorales@users.noreply.github.com>
2022-04-03 20:17:53 +0100

    Add files via upload

--- egg_people_unite_overlay.user.js
+++ egg_people_unite_overlay.user.js
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/kylomorales/rocketleagueplace/raw/main/2k_x_2k_overlay.png";
+            i.src = "https://github.com/kylomorales/rocketleagueplace/raw/main/2000x2000_overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
